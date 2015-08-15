ec5d2e27960b2baed6b69caf1ee28c0edbcf8534
Sérgio Alves Lima Jardim <sergio@plasmadesign.com.br>
2015-08-15 00:27:36 -0300

    First import

---
+++ src/javascript/bundle.js
@@ -0,0 +1,8 @@
+// Browserify entry point
+//var $ = require('jquery');
+//var velocity = require('velocity-animate');
+
+//Top nav mobile menu
+//To use, you need to uncomment the line below and also the jquery line (and install jquery of course)
+//var topnav = require('./top-nav');
+
---
+++ src/javascript/top-nav.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    alert("asdasd");
+    var menuToggle = $('#js-mobile-menu').unbind();
+    $('#js-navigation-menu').removeClass("show");
+
+    menuToggle.on('click', function(e) {
+        e.preventDefault();
+        $('#js-navigation-menu').slideToggle(function(){
+            if($('#js-navigation-menu').is(':hidden')) {
+                $('#js-navigation-menu').removeAttr('style');
+            }
+        });
+    });
+});
